63b18cc825fd5604b8add10303a3961afbe435ec
Jose Velasco <jose.juarez.velasco@gmail.com>
2023-04-10 14:02:28 -0700

    added Soffia's new code, updated code and files from the .zip,
    resolved issue that caused User not to get updated

--- src/application/controller/MainController.java
+++ src/application/controller/MainController.java
@@ -2,19 +2,74 @@ package application.controller;
 
 import java.io.IOException;
 
+import application.dal.UserDAOImpl;
+import application.dal.UserDAOInt;
+import application.model.User;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class MainController {
 	
-	@FXML
-	private Button resetPasswordButton;
-
-
+	@FXML private Button resetPasswordButton;
+	@FXML private Button login;
+	@FXML PasswordField passEnter;
+	
+	Boolean loginValidation = false;
+	
+	
+	@FXML public Boolean passwordEnterOp(){
+		try {
+			CommonObjs commonObjs = CommonObjs.getInstance(); 
+			UserDAOInt userDAO = new UserDAOImpl(commonObjs.getDataBaseObj().getConnection());
+			User user = userDAO.getUser();
+			
+			String newP = passEnter.getText(); // Get new password from user
+			if(newP.isEmpty()) {
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setHeaderText(null);
+				alert.setContentText("Please fill out all the requierd fields");
+				alert.showAndWait();
+			}
+			//p is default password, it is entered, user will be redirected to reset password
+			else if (newP.equals("p")||newP.equals(user.getPassword())){
+				loginValidation = true;
+				Stage stage = (Stage) login.getScene().getWindow();
+				stage.close();
+				Stage primaryStage = new Stage();
+				Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/ResetPassword.fxml"));
+				primaryStage.setScene(new Scene(root));
+				primaryStage.show();
+				
+			}
+		
+		} catch(Exception e) {
+			e.printStackTrace();
+			}
+		return loginValidation;
+	}
+	
+	@FXML public void loginClicked() {
+		if(passwordEnterOp()) {
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setHeaderText(null);
+			alert.setContentText("Successfully logged into the application");
+			alert.showAndWait();
+		}
+		else {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setHeaderText(null);
+			alert.setContentText("The password is incorrect");
+			alert.showAndWait();
+		}
+	}
+	
 	@FXML public void resetPasswordOp() {
 		
 		try {
@@ -30,5 +85,5 @@ public class MainController {
 			e.printStackTrace();
 		}
 	}
-
+	
 }
--- src/application/controller/ResetPasswordController.java
+++ src/application/controller/ResetPasswordController.java
@@ -2,6 +2,9 @@ package application.controller;
 
 import java.io.IOException;
 
+import application.dal.UserDAOImpl;
+import application.dal.UserDAOInt;
+import application.model.User;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -18,9 +21,11 @@ public class ResetPasswordController {
 	@FXML PasswordField newPasswordButton;
 	@FXML PasswordField oldPasswordButton;
 	@FXML Button resetPasswordButton;
+	@FXML Button newRecommendationButton;
 	@FXML Button goBackButton;
 	
 	Boolean oldPasswValidation = false;
+	
 	
 
 	@FXML public void logOutOp() {
@@ -43,6 +48,11 @@ public class ResetPasswordController {
 	@FXML public Boolean oldPasswordOp() {
 		
 		try {
+			
+			CommonObjs commonObjs = CommonObjs.getInstance(); 
+			UserDAOInt userDAO = new UserDAOImpl(commonObjs.getDataBaseObj().getConnection());
+			User user = userDAO.getUser();
+			
 			String oldPassw = oldPasswordButton.getText(); // Get old password from user
 			if(oldPassw.isEmpty()) {
 				Alert alert = new Alert(AlertType.ERROR);
@@ -50,14 +60,16 @@ public class ResetPasswordController {
 				alert.setContentText("Please fill out all the requierd fields");
 				alert.showAndWait();
 			}
-			//else if(oldPassw.equals("p") || oldPassw.equals(user.getPassword())) {
-			else if(oldPassw.equals("p") || oldPassw.equals("Zohreh")) {
+			
+			
+			else if (oldPassw.equals("p") || oldPassw.equals(user.getPassword())) {
 				oldPasswValidation = true;
 				}
 		
 		} catch (Exception e) {
 			e.printStackTrace();
 			}
+		
 		return oldPasswValidation;
 	}
 	
@@ -72,12 +84,8 @@ public class ResetPasswordController {
 				alert.setContentText("Please fill out all the requierd fields");
 				alert.showAndWait();
 			}
-			else if (newPasswordButton.getText().equals("Sasan")){
-				oldPasswValidation = true;
-				//user.setPassword();
-			}
 			else {
-				oldPasswValidation = false;
+				oldPasswValidation = true;
 			}
 					
 		} catch (Exception e) {
@@ -119,5 +127,24 @@ public class ResetPasswordController {
 			
 			e.printStackTrace();
 		}
+	}
+
+
+	@FXML public void newRecommendationOp() {
+		
+		//try {
+			Stage stage = (Stage) logoutButton.getScene().getWindow();
+			stage.close();
+			Stage primaryStage = new Stage();
+			primaryStage = RecommendationLetterSimulator.show(primaryStage);
+		
+		
+			primaryStage.show();
+			
+			
+	//	} catch (IOException e) {
+			
+		//	e.printStackTrace();
+		//}
 	}
 }
